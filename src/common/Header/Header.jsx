d5ee8d7e7134fb44397f2cc3335dafcbff817cbb
Jordi Jerico <jordijerico@gmail.com>
2023-03-17 13:42:19 +0100

    Estilos aplicados a login/register responsive movil y PC

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -4,10 +4,13 @@ import { Navigator } from '../Navigator/Navigator'
 import './Header.css'
 export const Header = () => {
     return (
-        <div  className="headerDesign">
-            
+        <div className="headerDesign">
+            <div className='imagen'></div>
+            <div className='linksNav'>
+            <Navigator ruta={"Home"} destino={"/"} />
             <Navigator ruta={"Login"} destino={"/login"} />
             <Navigator ruta={"Register"} destino={"/register"} />
+            </div>
         </div>
     )
 }
